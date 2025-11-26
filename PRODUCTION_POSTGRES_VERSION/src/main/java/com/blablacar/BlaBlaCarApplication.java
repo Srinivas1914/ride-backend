35012e9f06a46ab4d162ff8d6297614175f79673
Srinivas1914 <srinivasachyutha1914@gmail.com>
2025-11-27 01:02:40 +0530

    Update and rename BlaBlaCar Application.java to BlaBlaCarApplication.java

---
+++ PRODUCTION_POSTGRES_VERSION/src/main/java/com/blablacar/BlaBlaCarApplication.java
@@ -0,0 +1,11 @@
+package com.blablacar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlaBlaCarApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BlaBlaCarApplication.class, args);
+    }
+}
